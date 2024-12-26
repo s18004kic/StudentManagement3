c611d0cd4e3449a46e380766878db5006f198b5f
s18004kic <s18004@al.kic.ac.jp>
2024-12-26 09:27:21 +0900

    Updated StudentRepository with new features or fixes

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import raisetech.StudentManagement.data.Student;
@@ -28,7 +29,17 @@ public interface StudentRepository {
   @Insert(" INSERT INTO students_courses(student_id,course_name,course_start_at,course_end_at)"
       + "VALUES(#{studentId},#{courseName},#{courseStartAt},#{courseEndAt})")
   @Options(useGeneratedKeys = true,keyProperty = "id")
-  void registerStudentsCourses(StudentsCourses studentsCourses);
+
+  @Update(
+      "Update students SET (name = #{name},kana_name = #{kanaName},nickname = #{nickname},"
+          + "email = #{email},area = #{area},age = #{age},sex = #{sex},remark = #{remark},is_deleted = #{isDeleted}) WHERE id = #{id}")
+  void UpdateStudent(Student student);
+  @Update(
+      " Update students_courses(student_id,course_name,course_start_at,course_end_at)"
+      + "VALUES(#{studentId},#{courseName},#{courseStartAt},#{courseEndAt})")
+  void UpdateStudentsCourses(StudentsCourses studentsCourses);
 }
+
+
 
 
