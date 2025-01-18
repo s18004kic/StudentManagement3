54a302f37650d4ddb3a883c2e5514c14728fc98d
s18004kic <s18004@al.kic.ac.jp>
2025-01-18 10:51:43 +0900

    課題２２回目完了

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,23 +1,16 @@
 package raisetech.StudentManagement.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import raisetech.StudentManagement.controller.converter.StudentConverter;
-import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
@@ -38,9 +31,9 @@ public class StudentController {
   }
 
   /**
-   * 受講生一覧検索です。
-   * 全件検索を行うので、条件指定は行いません。
-   * @return　受講生一覧（全件）
+   * 受講生詳細の一覧検索です。全件検索を行うので、条件指定は行いません。
+   *
+   * @return　受講生詳細一覧（全件）
    */
 
   @GetMapping("/studentList")
@@ -49,8 +42,8 @@ public class StudentController {
   }
 
   /**
-   * 受講生検索です。
-   * IDに紐づく任意の受講生の情報を取得します。
+   * 受講生詳細の検索です。IDに紐づく任意の受講生の情報を取得します。
+   *
    * @param id　受講生ID
    * @return　受講生
    */
@@ -60,28 +53,31 @@ public class StudentController {
   }
 
   @GetMapping("/studentsCourseList")
-  public List<StudentsCourses> getStudentsCourseList(){
-    return service.searchStudentsCoursesList();
+  public List<StudentCourse> getStudentsCourseList(){
+    return service.searchStudentCourseList();
   }
 
-  //@GetMapping("/newStudent")
-  //public String newStudent(Model model){
-  //  StudentDetail studentDetail = new StudentDetail();
-  //  studentDetail.setStudentsCourses(Arrays.asList(new StudentsCourses()));
-  //  model.addAttribute("studentDetail", studentDetail);
-  //  return "registerStudent";
-  //}
-
+  /**
+   * 受講生詳細の登録を行います。
+   *
+   * @param studentDetail　受講生詳細
+   * @return　実行結果
+   */
   @PostMapping("/registerStudent")
   public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail){
     StudentDetail responseStudentDetail = service.registerStudent(studentDetail);
     return ResponseEntity.ok(responseStudentDetail);
   }
 
-  @PostMapping("/updateStudent")
+  /**
+   * 受講生詳細の更新を行います。キャンセルフラグの更新もここで行います（論理削除）
+   *
+   * @param studentDetail　受講生詳細
+   * @return　実行結果
+   */
+  @PutMapping("/updateStudent")
   public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
     return ResponseEntity.ok("更新処理が成功しました。");
   }
 }
-
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -6,10 +6,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentCourse;
 
 /**
  * 受講生テーブルと受講コース情報テーブルと紐づくrepositoryです。
@@ -39,7 +37,7 @@ public interface StudentRepository {
    * @return　受講生のコース情報（全件）
    */
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCoursesList(); //findAllとは別にStudentsCoursesを入れるのもＯＫ
+  List<StudentCourse> searchStudentCourseList(); //findAllとは別にStudentsCoursesを入れるのもＯＫ
 
   /**
    * 受講生IDに紐づく受講生コース情報を検索します。
@@ -48,30 +46,44 @@ public interface StudentRepository {
    * @return　受講生IDに紐づく受講生コース情報
    */
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentsCourses> searchStudentsCourses(String studentId);
+  List<StudentCourse> searchStudentCourse(String studentId);
 
+  /**
+   * 受講生を新規登録します。IDに関しては自動採番を行う。
+   * @param student　受講生
+   */
   @Insert(
       "INSERT INTO students(name,kana_Name,nickname,email,area,age,sex,remark,is_deleted) "
       + "VALUES(#{name},#{kanaName},#{nickname},#{email},#{area},#{age},#{sex},#{remark},false)")
   @Options(useGeneratedKeys = true,keyProperty = "id")
   void registerStudent(Student student);
 
+  /**
+   * 受講生コース情報を新規登録します。IDに関しては自動採番を行う。
+   * @param studentCourse　受講生コース情報
+   */
   @Insert(" INSERT INTO students_courses(student_id,course_name,course_start_at,course_end_at)"
       + "VALUES(#{studentId},#{courseName},#{courseStartAt},#{courseEndAt})")
   @Options(useGeneratedKeys = true,keyProperty = "id")
-  void registerStudentsCourse(StudentsCourses studentsCourses);
+  void registerStudentCourse(StudentCourse studentCourse);
 
+  /**
+   * 受講生を更新します。
+   *
+   * @param student　受講生
+   */
   @Update(
       "Update students SET name = #{name},kana_name = #{kanaName},nickname = #{nickname},"
           + "email = #{email},area = #{area},age = #{age},sex = #{sex},remark = #{remark},is_deleted = #{isDeleted} WHERE id = #{id}")
   void updateStudent(Student student);
 
-  @Update(
-      " Update students_courses SET course_name = #{courseName} WHERE id = #{id}")//(student_id,course_name,course_start_at,course_end_at)"
-      //+ "VALUES(#{studentId},#{courseName},#{courseStartAt},#{courseEndAt})")
-  void updateStudentsCourses(StudentsCourses studentsCourses);
-
-  //List<StudentsCourses> searchStudentsCoursesList();
+  /**
+   * 受講生コース情報のコース名を更新します。
+   *
+   * @param studentCourse　受講生コース情報
+   */
+  @Update("Update students_courses SET course_name = #{courseName} WHERE id = #{id}")
+  void updateStudentCourse(StudentCourse studentCourse);
 }
 
 
