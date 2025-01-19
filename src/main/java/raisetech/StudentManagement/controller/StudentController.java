070beec8178d5a6053afcd8b70211253350db416
s18004kic <s18004@al.kic.ac.jp>
2025-01-19 11:06:32 +0900

    課題２３回目完了

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,10 +1,15 @@
 package raisetech.StudentManagement.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import org.hibernate.engine.jdbc.Size;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.data.StudentCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
+import raisetech.StudentManagement.exception.TestException;
 import raisetech.StudentManagement.service.StudentService;
 
 /**
@@ -40,8 +46,10 @@ public class StudentController {
    */
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList(){
-    return service.searchStudentList();
+  public List<StudentDetail> getStudentList() throws TestException {
+    //return service.searchStudentList();
+    throw new TestException(
+        "現在のこのAPIは利用できません。URLは「studentList」ではなく「students」を利用ください。");
   }
 
   /**
@@ -51,7 +59,8 @@ public class StudentController {
    * @return　受講生
    */
   @GetMapping("/student/{id}")
-  public StudentDetail getStudent(@PathVariable String id){
+  public StudentDetail getStudent(
+      @PathVariable @NotBlank @Pattern(regexp = "^\\d+$") String id){
     return service.searchStudent(id);
   }
 
@@ -67,7 +76,8 @@ public class StudentController {
    * @return　実行結果
    */
   @PostMapping("/registerStudent")
-  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail){
+  public ResponseEntity<StudentDetail> registerStudent(
+      @RequestBody @Valid StudentDetail studentDetail){
     StudentDetail responseStudentDetail = service.registerStudent(studentDetail);
     return ResponseEntity.ok(responseStudentDetail);
   }
@@ -79,8 +89,13 @@ public class StudentController {
    * @return　実行結果
    */
   @PutMapping("/updateStudent")
-  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
+  public ResponseEntity<String> updateStudent(@RequestBody @Valid StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
     return ResponseEntity.ok("更新処理が成功しました。");
   }
+
+  //@ExceptionHandler(TestException.class)
+  //public ResponseEntity<String> handleTestException(TestException ex){
+  //  return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  //}
 }
