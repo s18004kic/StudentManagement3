7a64c98648c5a88543ecadff333e455f08a5fab9
s18004kic <s18004@al.kic.ac.jp>
2025-01-22 16:23:27 +0900

    課題２５回目完了

--- src/main/java/raisetech/StudentManagement/data/StudentCourse.java
+++ src/main/java/raisetech/StudentManagement/data/StudentCourse.java
@@ -1,14 +1,24 @@
 package raisetech.StudentManagement.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "受講生コース情報")
 @Getter
 @Setter
 public class StudentCourse {
+  @NotBlank
+  @Pattern(regexp = "^\\d+$")
   private String id;
+
+  @NotBlank
+  @Pattern(regexp = "^\\d+$")
   private String studentId;
+  @NotBlank
   private String courseName;
   private LocalDateTime courseStartAt;
   private LocalDateTime courseEndAt;
